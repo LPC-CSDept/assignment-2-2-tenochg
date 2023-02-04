23c2e20646cff0c49b413a87503f723af8914241
tenochg <yomoozuki@gmail.com>
2023-02-04 10:43:45 -0800

    2-2

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 def main():
     ##################################################
     # Comlete your code here
-    ##################################################
-    pass
+    ###################################################
+    temp_celsius = int(input('What is the temperature in Celsius:'))
+    celsius_to_F = 9/5 * temp_celsius + 32
+    print(f'Fahrenheit: {celsius_to_F:.2f}')
+
+pass
 
 
 if __name__ == '__main__':
